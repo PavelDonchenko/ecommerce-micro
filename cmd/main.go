252f22dfc6bf2f229a9d11e626c720c9d5729b3a
PavelDonchenko <przmld033@gmail.com>
2024-09-06 11:43:57 +0300

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var (
+	production   *bool
+	disableTrace *bool
+)
+
+func main() {
+	fmt.Println(production)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PavelDonchenko/ecommerce-micro
+
+go 1.23.0
